72020afdebb4ee9de89ef5ea5fd10b191a78263c
Pablo Ibarz <fribarz123@gmail.com>
2025-01-22 09:49:15 -0500

    Added multi-color damage holes

--- Scripts/Damage Model/DamageModelPlayerShadow.gd
+++ Scripts/Damage Model/DamageModelPlayerShadow.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var SHADOW_DIAMETER := 100.0
+var SHADOW_DIAMETER := 50.0
 
 @export var shadow:PackedScene
+@export var hole:PackedScene
 
 func _ready() -> void:
 	
@@ -10,7 +11,17 @@ func _ready() -> void:
 		set_physics_process(false)
 		Accessor.player_set.connect(func ():
 			set_physics_process(Accessor.player is Bullet)
+			attach_hole_pusher(Accessor.player)
 		)
+	else:
+		attach_hole_pusher(Accessor.player)
+
+func attach_hole_pusher(player:Bullet):
+	player.area_entered.connect(hole_pusher)
+
+func hole_pusher(area:Area2D):
+	if (area is Obstacle):
+		push_shadow_to_player_transform(hole)
 
 var last_shadow_position := Vector2.INF
 
@@ -19,13 +30,13 @@ func _physics_process(delta: float) -> void:
 	var distance_traveled := Accessor.player.position.\
 		distance_to(last_shadow_position)
 	if distance_traveled > SHADOW_DIAMETER:
-		push_shadow()
+		push_shadow_to_player_transform(shadow)
 		last_shadow_position = Accessor.player.position
 	
-func push_shadow() -> void:
-	if !shadow.can_instantiate(): return
+func push_shadow_to_player_transform(scene:PackedScene) -> void:
+	if scene == null || !scene.can_instantiate(): return
 	
-	var instance := shadow.instantiate()
+	var instance := scene.instantiate()
 	#print("adding shadow at", Accessor.player.position)
 	add_child(instance)
-	instance.position = Accessor.player.position
+	instance.transform = Accessor.player.transform
